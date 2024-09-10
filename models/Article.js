8109f5b69296d50cfef492060ad3f8ee317809a3
Syed Muhemin Ali <smuheminali@gmail.com>
2024-09-10 08:55:01 +0500

    updated code

--- models/Article.js
+++ models/Article.js
@@ -15,7 +15,6 @@ var articleSchema = new mongoose.Schema(
         },
         description: {
             type: String,
-            required: true,
         },
         price: {
             type: Number,
--- models/Product.js
+++ models/Product.js
@@ -15,7 +15,6 @@ var productSchema = new mongoose.Schema(
         },
         description: {
             type: String,
-            required: true,
         },
         price: {
             type: Number,
