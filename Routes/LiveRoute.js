c812d109fb44421e71c7d8ff5cb261327e891411
Syed Muhemin Ali <smuheminali@gmail.com>
2024-10-04 22:36:52 +0500

    updated code

--- Routes/LiveRoute.js
+++ Routes/LiveRoute.js
@@ -7,15 +7,14 @@ const {
     updateVideoId
 } = require("../Controller/liveStreamingController");
 const router = express.Router();
-const { checkRole, verifyToken } = require("../middleware/authMiddleware");
 
 
 
-router.route("/live").post(verifyToken, checkRole(['admin']), createVideoId);
-router.route("/live").get(verifyToken, checkRole(['admin', 'user']), getAllVideos);
-router.route("/live/:id").get(verifyToken, checkRole(['admin', 'user']), getVideoById);
-router.route("/live/:id").put(verifyToken, checkRole(['admin']), updateVideoId);
-router.route("/live/:id").delete(verifyToken, checkRole(['admin']), deleteVideoId);
+router.route("/live").post(createVideoId);
+router.route("/live").get(getAllVideos);
+router.route("/live/:id").get(getVideoById);
+router.route("/live/:id").put(updateVideoId);
+router.route("/live/:id").delete(deleteVideoId);
 
 
 
